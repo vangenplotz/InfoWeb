27f8f88037e44d222b372f0cb9cebe549e1d0022
Pål Messenlien <messenlien@gmail.com>
2012-10-18 09:01:04 +0200

    webview størrelse

--- src/com/vangplotz/infoweb/Browser.java
+++ src/com/vangplotz/infoweb/Browser.java
@@ -3,9 +3,6 @@ package com.vangplotz.infoweb;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.view.View;
-import android.view.Window;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 @SuppressLint("SetJavaScriptEnabled") public class Browser extends Activity {
@@ -15,15 +12,13 @@ import android.webkit.WebView;
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().getDecorView().setSystemUiVisibility
-        (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.activity_browser);
 
 		webView = (WebView) findViewById(R.id.webView1);
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
+		webView.setInitialScale(95);
 
+		webView.getSettings().setJavaScriptEnabled(true);
+		
 		webView.loadUrl("https://web.timeedit.se/hil_no/db1/dagens/r.html?base=c2lkPTMmb2JqZWN0cz0xNjExNTUuMTg1JTJDMTYxMTU2LjE4NSUyQzE2MTE1Ny4xODUlMkMxNjExNTguMTg1JTJDMTYxMTU5LjE4NSUyQzE2MTE2MC4xODUlMkMxNjExNjEuMTg1JTJDMTYxMTYyLjE4NSUyQzE2MTE5OS4xODUlMkMxNjEyMDAuMTg1JTJDMTYxMjAxLjE4NSUyQzE2MTIwMi4xODUlMkMxNjEyMDMuMTg1JTJDMTYxMjA1LjE4NSUyQzE2MTIwNC4xODUlMkMxNjEyMTIuMTg1JTJDMTYxMjEzLjE4NSUyQzE2MTE5Ni4xODUlMkMxNjExOTcuMTg1JTJDMTYxMTk4LjE4NSZveD0wJmgyPWYmY2NoPTE4MiUyQzE4OS0rLmY0MS1Lb21tZW50YXIuZjItKy5mNDctKyZwPTAuZGF5cyUyQzAuZGF5cyZoPWY_");  
     }
 }
