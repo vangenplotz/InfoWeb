b09f5b4441f88f728a7114ec8ac69b5501cb527e
Pål Messenlien <messenlien@gmail.com>
2012-10-23 11:06:52 +0200

    Roterig

--- src/com/vangplotz/infoweb/Browser.java
+++ src/com/vangplotz/infoweb/Browser.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -23,20 +24,20 @@ public class Browser extends Activity {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_browser);
         // Go full screen
         	final Window window = getWindow();
         	window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		webView = (WebView) findViewById(R.id.webView1);
-		webView.loadUrl("http://app.infoskjermen.no/MKI569");  
+		// webView.loadUrl("http://app.infoskjermen.no/MKI569");  
+		webView.loadUrl("https://web.timeedit.se/hil_no/db1/dagens/r.html?base=c2lkPTMmb2JqZWN0cz14LjcuQXVkaXRvcml1bS5LbGFzc2Vyb20uMTg1LiZveD0wJmNjaD0xODIlMkMxODktKy5mNDEtS29tbWVudGFyLmYyLSsuZjQ3LSsmcD0wLm1pbnV0ZXMlMkMwLmRheXMmaD1mJmgyPWY_ "); 
 		webView.getSettings().setJavaScriptEnabled(true);
         webView.setKeepScreenOn(true);
-		webView.setInitialScale(90);
+		webView.setInitialScale(110);
 		webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
-
-   		new ReloadWebView(this, 4000, webView);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+   		new ReloadWebView(this, 60, webView);
     }
 }
