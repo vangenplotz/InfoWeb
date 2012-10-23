b9e9c73a2e6997537b6c0e97d27252ef1e103eff
Pål Messenlien <messenlien@gmail.com>
2012-10-23 11:33:00 +0200

    Portrett visning

--- src/com/vangplotz/infoweb/Browser.java
+++ src/com/vangplotz/infoweb/Browser.java
@@ -29,15 +29,16 @@ public class Browser extends Activity {
         	final Window window = getWindow();
         	window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		webView = (WebView) findViewById(R.id.webView1);
-		// webView.loadUrl("http://app.infoskjermen.no/MKI569");  
-		webView.loadUrl("https://web.timeedit.se/hil_no/db1/dagens/r.html?base=c2lkPTMmb2JqZWN0cz14LjcuQXVkaXRvcml1bS5LbGFzc2Vyb20uMTg1LiZveD0wJmNjaD0xODIlMkMxODktKy5mNDEtS29tbWVudGFyLmYyLSsuZjQ3LSsmcD0wLm1pbnV0ZXMlMkMwLmRheXMmaD1mJmgyPWY_ "); 
+		webView.loadUrl("http://app.infoskjermen.no/PAL831");  
+		//webView.loadUrl("https://web.timeedit.se/hil_no/db1/dagens/r.html?base=c2lkPTMmb2JqZWN0cz14LjcuQXVkaXRvcml1bS5LbGFzc2Vyb20uMTg1LiZveD0wJmNjaD0xODIlMkMxODktKy5mNDEtS29tbWVudGFyLmYyLSsuZjQ3LSsmcD0wLm1pbnV0ZXMlMkMwLmRheXMmaD1mJmgyPWY_"); 
 		webView.getSettings().setJavaScriptEnabled(true);
         webView.setKeepScreenOn(true);
 		webView.setInitialScale(110);
 		webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
    		new ReloadWebView(this, 60, webView);
     }
 }
